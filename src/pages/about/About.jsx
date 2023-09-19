3d7d117af685f8646c84924f97df7881ad5bf423
felixop7 <roshansahani226@gmail.com>
2023-09-19 18:58:34 +0545

    contacts

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
     <div className="about">
       <img src={images.vert_icon} alt="logo" className="about__logo" />
       <p className="about__content">
-        Event Hive is an inter college event management system that aims to
+        KUvents is a college event management system that aims to
         provide a comprehensive platform for colleges to organize, manage and
         promote their events. The platform aims to streamline the event
         management process by providing tools and features to effectively plan,
@@ -17,7 +17,7 @@ const About = () => {
         audience.
         <br />
         <br />
-        Event Hive is designed to be user-friendly, flexible, and customizable
+        KUvents is designed to be user-friendly, flexible, and customizable
         to meet the specific needs of different colleges. The platform enables
         colleges to manage multiple events simultaneously and provides real-time
         updates on event progress. By leveraging the latest technologies, Event
@@ -43,12 +43,12 @@ const About = () => {
                 color: "#7848F4",
               }}
             >
-              Prashant
+              Roshan
             </span>{" "}
-            Kumar Singh
+            Sahani
           </h1>
           <p className="about__creator__info__content">
-            Greetings from Prashant Kumar Singh, the creator of this website! I
+            Greetings from Roshan Sahani, the creator of this website! I
             extend a warm welcome to all our visitors and users. It brings me
             joy to see you here, and I hope you have a wonderful experience
             exploring our platform. As the creator, I am dedicated to providing
@@ -57,18 +57,18 @@ const About = () => {
             input is valuable to me, and I am committed to continuously
             improving the website to meet your needs. Thank you for choosing our
             platform. Enjoy your time here! <br />
-            <br /> Best regards, <br /> Prashant Kumar Singh
+            <br /> Best regards, <br /> Roshan Sahani
           </p>
           <div className="about__creator__info__links">
             <a
-              href="https://www.linkedin.com/in/devprashantt/"
+              href="https://www.linkedin.com/in/felix.hud/"
               target="_blank"
               rel="noopener noreferrer"
             >
               <img src={images.linkedin} alt="event-hive" />
             </a>
             <a
-              href="https://twitter.com/devprashantt"
+              href="https://twitter.com/felix.hud"
               target="_blank"
               rel="noopener noreferrer"
             >
@@ -96,7 +96,7 @@ const About = () => {
               <img src={images.medium} alt="event-hive" />
             </a>
             <a
-              href="https://github.com/devprashantt"
+              href="https://github.com/felixop7"
               target="_blank"
               rel="noopener noreferrer"
             >
